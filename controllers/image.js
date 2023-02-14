fccd7aa912b5a700d725439aeec3f762f715f31a
Waldemar Hermann <hermann.waldemar19995@gmail.com>
2023-02-14 21:11:55 +0100

    first commit

---
+++ controllers/image.js
@@ -0,0 +1,38 @@
+const Clarifai = require('clarifai');
+
+const app = new Clarifai.App({
+    apiKey: '4dab164a20414f9d88f24f50a2c7ccfe'
+});
+
+const handleApiCall = (req, res) => {
+    app.models
+        .predict(
+            {
+                id: 'face-detection',
+                name: 'face-detection',
+                version: '6dc7e46bc9124c5c8824be4822abe105',
+                type: 'visual-detector',
+            }, req.body.input)
+        .then(data => {
+            res.json(data);
+        })
+        .catch(err => res.status(400).json('unable to connect to API'))
+}
+
+
+
+const handleImage = (req, res, db) => {
+    const { id } = req.body;
+    db('users').where('id', '=', id)
+        .increment('entries', 1)
+        .returning('entries')
+        .then(entries => {
+            res.json(entries[0].entries);
+        })
+        .catch(err => res.status(400).json('error'));
+}
+
+module.exports = {
+    handleImage,
+    handleApiCall
+}
---
+++ controllers/signin.js
@@ -0,0 +1,27 @@
+
+const handleSignin = (db, bcrypt) => (req, res) => {
+    const {email, password} = req.body;
+    if (!email || !password) {
+        return res.status(400).json('input is wrong')
+    } 
+    db.select('email', 'hash').from('login')
+        .where('email', '=', email)
+        .then(data => {
+            const isValid = bcrypt.compareSync(password, data[0].hash);
+            if (isValid) {
+                return db.select('*').from('users')
+                    .where('email', '=', email)
+                    .then(user => {
+                        res.json(user[0])
+                    })
+                    .catch(err => res.status(400).json('error: unable to find user'))
+            } else {
+                res.status(400).json('wrong login informations')
+            }
+        })
+        .catch(err => res.status(400).json('wrong login informations'))
+}
+
+module.exports = {
+    handleSignin: handleSignin
+}
